179f454e0c3250bf4b652316462ce318a2fe220d
Michael Hoyt <economyofdreams@gmail.com>
2020-04-03 09:27:41 -0400

    Fixed Watchdog not working recursively

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import click
 import markdown
 from jinja2 import Environment, PackageLoader, BaseLoader
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 from yaml import load, dump
 
 try:
@@ -17,8 +17,39 @@ except ImportError:
 
 
 
-class Event(LoggingEventHandler):
-    def on_modified(self, event):
+class Watcher:
+    DIRECTORY_TO_WATCH = "./assets"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("Received modified event - %s." % event.src_path)
+
         builder()
 
 
@@ -82,16 +113,8 @@ def build():
 
 @cli.command()
 def watch():
-    event_handler = Event()
-    observer = Observer()
-    observer.schedule(event_handler, './assets', recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    w = Watcher()
+    w.run()
 
 
 if __name__ == '__main__':
